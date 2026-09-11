0d6e7d8256e2b778779edbb4bbc9dbefcaad35ea
aadi9792 <adityasingh998466@gmail.com>
2026-09-11 12:09:44 +0530

    shopping app

--- Shopping-App/Shopping-App/client/src/components/Footer.jsx
+++ Shopping-App/Shopping-App/client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="footer">
-        <h2>Design & Developed by Aditya Yadav</h2>
+        <h2>Design & Developed by Aditya Singh</h2>
     </div>
   )
 }
